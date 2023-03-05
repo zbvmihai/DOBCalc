f8500f47a1759d4ca048ef57d5643e0410ef454a
Zabi <zbvmihai@gmail.com>
2023-03-05 11:16:08 +0000

    add date picker functionality

--- app/src/main/java/com/zabava/dobcalc/MainActivity.kt
+++ app/src/main/java/com/zabava/dobcalc/MainActivity.kt
@@ -4,14 +4,44 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    private fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(
+            this,
+            { view, selectedYear, selectedMonth, selectedDay ->
+
+                val selectedDate = "$selectedDay/${selectedMonth+1}/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+            },
+            year,
+            month,
+            day
+        ).show()
 
     }
 
