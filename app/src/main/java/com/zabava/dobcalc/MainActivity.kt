f0832dca1c4c4076677b69998a10842fc6f83714
Zabi <zbvmihai@gmail.com>
2023-03-05 11:00:36 +0000

    add application design

--- app/src/main/java/com/zabava/dobcalc/MainActivity.kt
+++ app/src/main/java/com/zabava/dobcalc/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.zabava.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 }
